f5e62aaf7402592d224a1419360a12bf6dcfa6e5
NamanHegde38 <namanhegde33@gmail.com>
2018-12-15 13:22:06 +0100

    Revert "Fortnite 2D 0.2"
    This reverts commit 99eb8bf6a13c21a6747f1f9a79c75890e013dd03.

---
+++ Fortnite 2D/Assets/Scripts/Player/AttackTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Script by AquaArmour
+
+public class AttackTrigger : MonoBehaviour {
+
+	
+
+	void Start () {
+		
+		
+	}
+	
+	void Update () {
+		
+		
+	}
+}
---
+++ Fortnite 2D/Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Script by AquaArmour
+
+public class PlayerAttack : MonoBehaviour {
+
+	
+
+	void Start () {
+		
+		
+	}
+	
+	void Update () {
+		
+		
+	}
+}

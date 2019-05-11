8257d238758c0b3258fc6cb96c00c482c885e376
NamanHegde38 <namanhegde33@gmail.com>
2019-05-11 14:59:29 +0200

    Fortnite 2D 0.3.2 (6)
    Episode 4 and 5 of Sebastian Lague character controller - Basic character controller completed

--- Fortnite 2D/Assets/Scripts/Player/PlayerController.cs
+++ Fortnite 2D/Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour {
 	public int horizontalRayCount = 4;
 	public int verticalRayCount = 4;
 
+	float maxClimbAngle = 60;
+	float maxDescendAngle = 60;
+
 	float horizontalRaySpacing;
 	float verticalRaySpacing;
 
@@ -27,8 +30,12 @@ public class PlayerController : MonoBehaviour {
 	public void Move (Vector3 velocity) {
 
 		UpdateRaycastOrigins();
-
 		collisions.Reset();
+		collisions.velocityOld = velocity;
+
+		if (velocity.y < 0) {
+			DescendSlope(ref velocity);
+		}
 
 		if (velocity.x != 0) {
 			HorizontalCollisions(ref velocity);
@@ -55,11 +62,39 @@ public class PlayerController : MonoBehaviour {
 			Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
 
 			if (hit) {
-				velocity.x = (hit.distance - skinWidth) * directionX;
-				rayLength = hit.distance;
 
-				collisions.left = directionX == -1;
-				collisions.right = directionX == 1;
+				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+				if (i == 0 && slopeAngle <= maxClimbAngle) {
+
+					if (collisions.descendingSlope) {
+						collisions.descendingSlope = false;
+						velocity = collisions.velocityOld;
+					}
+
+					float distanceToSlopeStart = 0;
+
+					if (slopeAngle != collisions.slopeAngleOld) {
+						distanceToSlopeStart = hit.distance - skinWidth;
+						velocity.x -= distanceToSlopeStart * directionX;
+					}
+
+					ClimbSlope(ref velocity, slopeAngle);
+					velocity.x += distanceToSlopeStart * directionX;
+				}
+				
+				if (!collisions.climbingSlope || slopeAngle > maxClimbAngle) {
+
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					rayLength = hit.distance;
+
+					if (collisions.climbingSlope) {
+						velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+					}
+
+					collisions.left = directionX == -1;
+					collisions.right = directionX == 1;
+				}
 			}
 		}
 	}
@@ -78,11 +113,73 @@ public class PlayerController : MonoBehaviour {
 			Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
 
 			if (hit) {
+
 				velocity.y = (hit.distance - skinWidth) * directionY;
 				rayLength = hit.distance;
 
+				if (collisions.climbingSlope) {
+					velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+				}
+
 				collisions.below = directionY == -1;
 				collisions.above = directionY == 1;
+			}
+		}
+
+		if (collisions.climbingSlope) {
+
+			float directionX = Mathf.Sign(velocity.x);
+			rayLength = Mathf.Abs(velocity.x) + skinWidth;
+			Vector2 rayOrigin = ((directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight) + Vector2.up * velocity.y;
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+
+			if (hit) {
+
+				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+				if (slopeAngle != collisions.slopeAngle) {
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					collisions.slopeAngle = slopeAngle;
+				}
+			}
+		}
+	}
+
+	void ClimbSlope (ref Vector3 velocity, float slopeAngle) {
+
+		float moveDistance = Mathf.Abs(velocity.x);
+		float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+		if (velocity.y <= climbVelocityY) {
+
+			velocity.y = climbVelocityY;
+			velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+			collisions.below = true;
+			collisions.climbingSlope = true;
+			collisions.slopeAngle = slopeAngle;
+		}	
+	}
+
+	void DescendSlope (ref Vector3 velocity) {
+
+		float directionX = Mathf.Sign(velocity.x);
+		Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+		RaycastHit2D hit = Physics2D.Raycast(rayOrigin, -Vector2.up, Mathf.Infinity, collisionMask);
+
+		if (hit) {
+			float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+			if (slopeAngle != 0 && slopeAngle <= maxDescendAngle) {
+				if (Mathf.Sign(hit.normal.x) == directionX) {
+					if (hit.distance - skinWidth <= Mathf.Tan(slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x)) {
+						float moveDistance = Mathf.Abs(velocity.x);
+						float descendVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+						velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+						velocity.y -= descendVelocityY;
+
+						collisions.slopeAngle = slopeAngle;
+						collisions.descendingSlope = true;
+						collisions.below = true;
+					}
+				}
 			}
 		}
 	}
@@ -121,10 +218,20 @@ public class PlayerController : MonoBehaviour {
 		public bool above, below;
 		public bool left, right;
 
+		public bool climbingSlope;
+		public bool descendingSlope;
+		public float slopeAngle, slopeAngleOld;
+		public Vector3 velocityOld;
+
 		public void Reset () {
 
 			above = below = false;
 			left = right = false;
+			climbingSlope = false;
+			descendingSlope = false;
+
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0;
 		}
 	}
 }
--- Fortnite 2D/Assets/Scripts/Player/PlayerMovement.cs
+++ Fortnite 2D/Assets/Scripts/Player/PlayerMovement.cs
@@ -18,15 +18,18 @@ public class PlayerMovement : MonoBehaviour {
 	Vector3 velocity;
 	float velocityXSmoothing;
 
+	bool facingRight = true;
+
 	PlayerController controller;
+	Animator anim;
 
 	void Start () {
 
 		controller = GetComponent<PlayerController>();
+		anim = GetComponent<Animator>();
 
 		gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-		print("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
 	}
 
 	void Update () {
@@ -37,13 +40,40 @@ public class PlayerMovement : MonoBehaviour {
 
 		Vector2 input = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+		anim.SetFloat("Speed", Mathf.Abs(velocity.x));
+
 		if (Input.GetButtonDown("Jump") && controller.collisions.below) {
 			velocity.y = jumpVelocity;
+		}
+
+		if (!controller.collisions.below) {
+			anim.SetBool("Jumping", true);
+		}
+
+		else {
+			anim.SetBool("Jumping", false);
 		}
 
 		float targetVelocityX = input.x * moveSpeed;
 		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move(velocity * Time.deltaTime);
+
+		if (velocity.x > 0 && !facingRight) {
+			Flip();
+		}
+
+		if (velocity.x < 0 && facingRight) {
+			Flip();
+		}
+	}
+
+	void Flip () {
+
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }

066785c1f160d7faf802cc0af653b7cb953b0e89
NamanHegde38 <namanhegde33@gmail.com>
2019-11-30 22:36:19 +0100

    Splatoon 2D 0.04
    Implemented weapon rotation

---
+++ Splatoon 2D/Assets/Scripts/Misc/FrameRateHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Misc {
+    public class FrameRateHandler : MonoBehaviour {
+
+        [SerializeField] private int frameRateLimit;
+        [SerializeField] private float updateInterval;
+        [SerializeField] private TextMeshProUGUI frameText;
+        
+        private int _frameRate;
+        private float _updateTimer;
+        
+        private void Awake() {
+            Application.targetFrameRate = frameRateLimit;
+        }
+
+        private void Update() {
+            if (_updateTimer <= 0) {
+                _frameRate = (int)(1f / Time.unscaledDeltaTime);
+                frameText.text = _frameRate + " Fps";
+                _updateTimer = updateInterval;
+            }
+            else {
+                _updateTimer -= Time.deltaTime;
+            }
+            
+            
+        }
+    }
+}
